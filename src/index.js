531939f94387ffa65fe9c4ed262f10772f6230c9
gongchengxiang <gcx5291@163.com>
2022-06-19 23:30:39 +0800

    d

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.scss";
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+const TestFetch = async () => {
+  try {
+    const res = await fetch("http://180.76.242.137:3000/todo");
+    alert("fetch success");
+  } catch (error) {
+    alert("fetch failed");
+  }
+};
+TestFetch();
